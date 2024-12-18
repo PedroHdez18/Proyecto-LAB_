f1a3f34ccbd5eca10c11b46cfbd6408a8be77b28
PedroHdez18 <alu0101423944@ull.edu.es>
2024-12-18 18:46:46 +0000

    nuevos tests

--- src/test/java/poo/food/apple/AppleTest.java
+++ src/test/java/poo/food/apple/AppleTest.java
@@ -16,31 +16,31 @@ public class AppleTest {
 //        setFinalStatic(Config.class.getDeclaredField("width"), 400);
 //        setFinalStatic(Config.class.getDeclaredField("height"), 400);
 //    }
-//
+
 //    private void setFinalStatic(Field field, Object newValue) throws Exception {
 //        field.setAccessible(true);
 //        field.set(null, newValue);
 //    }
 //
-//    @Test
-//    public void testAppleCreation() {
-//        // Create an Apple instance
-//        Apple apple = new Apple();
-//
-//        // Verify the apple is created and is not null
-//        assertNotNull(apple.getApple());
-//
-//        // Verify the size of the canvas
-//        Canvas appleCanvas = apple.getApple();
-//        assertEquals((double) Config.squareSize, appleCanvas.getWidth());
-//        assertEquals((double) Config.squareSize, appleCanvas.getHeight());
-//
-//        // Verify the position of the apple is within the game area
-//        int posX = apple.getPosicionX();
-//        int posY = apple.getPosicionY();
-//        assertTrue(posX >= 0 && posX < Config.width);
-//        assertTrue(posY >= 0 && posY < Config.height);
-//    }
+////    @Test
+////    public void testAppleCreation() {
+////        // Create an Apple instance
+////        Apple apple = new Apple();
+////
+////        // Verify the apple is created and is not null
+////        assertNotNull(apple.getApple());
+////
+////        // Verify the size of the canvas
+////        Canvas appleCanvas = apple.getApple();
+////        assertEquals((double) Config.squareSize, appleCanvas.getWidth());
+////        assertEquals((double) Config.squareSize, appleCanvas.getHeight());
+////
+////        // Verify the position of the apple is within the game area
+////        int posX = apple.getPosicionX();
+////        int posY = apple.getPosicionY();
+////        assertTrue(posX >= 0 && posX < Config.width);
+////        assertTrue(posY >= 0 && posY < Config.height);
+////    }
 //
 //    @Test
 //    public void testSetRandomPosition() {
