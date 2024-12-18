09ac43cb18447a97efd6ab1da76de4a438e6d3a6
PedroHdez18 <alu0101423944@ull.edu.es>
2024-12-18 18:40:26 +0000

    creando nuevos tests

--- src/test/java/poo/canvas_utils_square/CanvasUtilsSquaresTest.java
+++ src/test/java/poo/canvas_utils_square/CanvasUtilsSquaresTest.java
@@ -9,17 +9,17 @@ import poo.config.Config;
 
 public class CanvasUtilsSquaresTest {
 
-//    @Test
-//    public void testBuildingSquare() {
-//        Paint color = Color.RED;
-//        Canvas canvas = CanvasUtilsSquares.buildingSquare(color);
-//
-//        // Verify the size of the canvas
-//        assertEquals((double) Config.squareSize, canvas.getWidth());
-//        assertEquals((double) Config.squareSize, canvas.getHeight());
-//
-//        // Verify the color of the square (this is a bit tricky since we can't directly access the color)
-//        // We can check if the GraphicsContext has the correct fill color set
-//        assertEquals(color, canvas.getGraphicsContext2D().getFill());
-//    }
+    @Test
+    public void testBuildingSquare() {
+        Paint color = Color.RED;
+        Canvas canvas = CanvasUtilsSquares.buildingSquare(color);
+
+        // Verify the size of the canvas
+        assertEquals((double) Config.squareSize, canvas.getWidth());
+        assertEquals((double) Config.squareSize, canvas.getHeight());
+
+        // Verify the color of the square (this is a bit tricky since we can't directly access the color)
+        // We can check if the GraphicsContext has the correct fill color set
+        assertEquals(color, canvas.getGraphicsContext2D().getFill());
+    }
 }
